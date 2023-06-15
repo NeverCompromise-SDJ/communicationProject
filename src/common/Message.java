5eac268828639a828c849ac997c5deeddb290a2c
sdj <1580987935@qq.com>
2023-06-15 10:59:14 +0800

    创建Message和User

--- src/common/Message.java
+++ src/common/Message.java
@@ -1,4 +1,59 @@
 package common;
 
-public class Message {
+import java.io.Serializable;
+
+/**
+ * 客户端和服务端通信时的消息对象
+ */
+public class Message implements Serializable {
+    private static final Long serialVersionUID = 1L;//序列化ID
+    String sender;//发送者
+    String getter;//接收者
+    String content;//内容
+    String sendTime;//发送时间
+    String msgType;//消息类型
+
+    public static Long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getGetter() {
+        return getter;
+    }
+
+    public void setGetter(String getter) {
+        this.getter = getter;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(String sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
 }
---
+++ src/common/User.java
@@ -0,0 +1,33 @@
+package common;
+
+import java.io.Serializable;
+
+/**
+ * 表示一个用户信息
+ */
+public class User implements Serializable {
+    private static final Long serialVersionUID = 1L;//序列化ID
+    private String userId;//用户ID
+    private String passwd;//用户密码
+
+    public User(String userId, String passwd) {
+        this.userId = userId;
+        this.passwd = passwd;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
