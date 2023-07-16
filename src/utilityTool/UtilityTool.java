8a7f7652643417dfe9cf065b462425e9172b7157
NeverCompromise-SDJ <1580987935@qq.com>
2023-07-16 18:23:55 +0800

    UtilityTool类更新，改善了时间的格式

--- src/utilityTool/UtilityTool.java
+++ src/utilityTool/UtilityTool.java
@@ -5,21 +5,15 @@ package utilityTool;
  * @create 2023/7/11 - 9:04
  */
 
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 工具类，提供通用方法来完成一些经常出现的操作
  */
 public class UtilityTool {
     public static String getLocalTime() {
-        LocalDateTime sendTime = LocalDateTime.now();
-        String sendTimeOfYear = String.valueOf(sendTime.getYear());
-        String sendTimeOfMonth = String.valueOf(sendTime.getMonthValue());
-        String sendTimeOfDay = String.valueOf(sendTime.getDayOfMonth());
-        String sendTimeOfHour = String.valueOf(sendTime.getHour());
-        String sendTimeOfMinute = String.valueOf(sendTime.getMinute());
-        String sendTimeOfSecond = String.valueOf(sendTime.getSecond());
-        return sendTimeOfYear + "-" + sendTimeOfMonth + "-" + sendTimeOfDay + " " + sendTimeOfHour + ":" +
-                sendTimeOfMinute + ":" + sendTimeOfSecond;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(new Date());
     }
 }
